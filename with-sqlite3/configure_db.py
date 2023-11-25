4d222e83c7cbc41bdd9330c56a117d54f11ca74d
el0qu4nce <135351108+el0qu4nce@users.noreply.github.com>
2023-11-26 01:05:14 +0300

    Update configure_db.py

--- with-sqlite3/configure_db.py
+++ with-sqlite3/configure_db.py
@@ -3,3 +3,8 @@ you should have "phone_dict.db" file already created, but empty inside
 further we will configure lines and columns of it
 """
 
+import sqlite3
+
+connection = sqlite3.connect("phone_dict.db")
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE phonenumbers (phonenumber TEXT, name TEXT)")
