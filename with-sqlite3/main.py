b485b7d70689e68e0b72f237ff3262667f7f18ca
el0qu4nce <135351108+el0qu4nce@users.noreply.github.com>
2023-11-26 00:52:46 +0300

    Update main.py
    adding v1.01 main.py

--- with-sqlite3/main.py
+++ with-sqlite3/main.py
@@ -1,1 +1,22 @@
 import sqlite3
+
+func = int(input("Do you want to add person (1) or find person?(2)"))
+if func == 1:
+    name = input("Enter the person's name: ")
+    number = input("Enter the person's phonenumber: ")
+    connection = sqlite3.connect("phone_dict.db")
+    cursor = connection.cursor()
+    params = (number, name)
+    cursor.execute(f"INSERT INTO phonenumbers VALUES ({number}, {name})")
+    connection.commit()
+    connection.close()
+    print("Ready!")
+else:
+    name = input("Enter the person's name: ")
+    connection = sqlite3.connect("phone_dict.db")
+    cursor = connection.cursor()
+    rows = cursor.execute("SELECT * FROM phonenumbers").fetchall()
+    for i in rows:
+        if i[0] == name:
+            print(f"There is {i[1]} with phonenumber {i[0]}")
+            break
